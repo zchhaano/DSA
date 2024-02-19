9303e11431a15455b755c2fd11fa43bd0bbac483
Zhang Chao (BEG/ESF3) <chao.zhang2@de.bosch.com>
2024-02-19 21:55:55 +0100

    2024.02.19

--- ParseTree.py
+++ ParseTree.py
@@ -30,12 +30,14 @@ def buildParseTree(fpexp):
     return eTree
 
 def evaluate(parseTree):
-    opers = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+    opers = {'+': operator.add, '-': operator.sub, 
+             '*': operator.mul, '/': operator.truediv}
     leftC = parseTree.getLeftChild()
     rightC = parseTree.getRightChild()
 
     if leftC and rightC:
         fn = opers[parseTree.getRootVal()]
-        return fn(evaluate(leftC), evaluate(rightC))
+        return fn(evaluate(leftC), 
+                  evaluate(rightC))
     else:
         return parseTree.getRootVal()
