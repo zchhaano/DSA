9daed25dc439adcec71de94472bbd5b5c3db1eac
Zhang Chao (BEG/ESF3) <chao.zhang2@de.bosch.com>
2024-02-19 19:03:28 +0100

    2024.02.19

--- sorting.py
+++ sorting.py
@@ -1,16 +1,114 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+#another way
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+#a better way                
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist)-1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                exchanges = True
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+        passnum = passnum -1
+
+#selection sort
+def selectionSort(alist):
+    for fillslot in range(len(alist)-1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, fillslot+1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location
+
+        temp = alist[fillslot]
+        alist[fillslot] = alist[positionOfMax]
+        alist[positionOfMax] = temp
 #%%
+#insertion sort
+def insertionSort(alist):
+    for index in range(1, len(alist)):
+        currentvalue = alist[index]
+        position = index
+
+        while position > 0 and alist[position-1] > currentvalue:
+            alist[position] = alist[position-1]
+            position = position - 1
+
+        alist[position] = currentvalue
+
+# %%
+def shellSort(alist):
+    sublistcount = len(alist) // 2
+    while sublistcount > 0:
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist, startposition, sublistcount)
+        print("After increments of size", sublistcount, "The list is", alist)
+        sublistcount = sublistcount // 2
+
+def gapInsertionSort(alist, start, gap):
+    for i in range(start + gap, len(alist), gap):
+        currentvalue = alist[i]
+        position = i
+        while position >= gap and alist[position - gap] > currentvalue:
+            alist[position] = alist[position - gap]
+            position = position - gap
+        alist[position] = currentvalue
+#%%
+
+def mergeSort(alist):
+    print("Splitting ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+        print('lefthalf:', lefthalf, 'righthalf:', righthalf)
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        #至此假设lefthalf和righthalf都是有序的
+        #下面将两个小的有序列表归并为一个大的有序列表
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+    print("Merging ", alist)        
+
+# %%
 def quickSort(alist):
     quickSortHelper(alist, 0, len(alist)-1)
-    print(alist)
-def quickSortHelper(alist, first, last):
-    """
-    Helper function for quicksort algorithm.
 
-    Parameters:
-    alist (list): The list to be sorted.
-    first (int): The index of the first element in the sublist to be sorted.
-    last (int): The index of the last element in the sublist to be sorted.
-    """
+def quickSortHelper(alist, first, last):
     if first < last:
         splitpoint = partition(alist, first, last)
         quickSortHelper(alist, first, splitpoint-1)
@@ -21,45 +119,20 @@ def partition(alist, first, last):
     leftmark = first + 1
     rightmark = last
     done = False
-
     while not done:
         while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
             leftmark = leftmark + 1
-
         while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
             rightmark = rightmark - 1
-
         if rightmark < leftmark:
             done = True
         else:
             temp = alist[leftmark]
             alist[leftmark] = alist[rightmark]
             alist[rightmark] = temp
-
     temp = alist[first]
     alist[first] = alist[rightmark]
     alist[rightmark] = temp
-
     return rightmark
+#%%
 
-# %%
-#使用三数取中法避免切分不均匀，即在选择 基准值时考虑列表的头元素、中间元素与尾元素
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    else:
-        pivot = median_of_three(arr[0], arr[len(arr) // 2], arr[-1])
-        less = [x for x in arr if x < pivot]
-        equal = [x for x in arr if x == pivot]
-        greater = [x for x in arr if x > pivot]
-        return quicksort(less) + equal + quicksort(greater)
-
-def median_of_three(a, b, c):
-    if (a - b) * (c - a) >= 0:
-        return a
-    elif (b - a) * (c - b) >= 0:
-        return b
-    else:
-        return c
-
-# %%
