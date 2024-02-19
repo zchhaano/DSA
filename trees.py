1db85a6bcab4791498301a3548858b3a0e398df3
Zhang Chao (BEG/ESF3) <chao.zhang2@de.bosch.com>
2024-02-19 19:12:36 +0100

    2024.02.19

--- trees.py
+++ trees.py
@@ -70,4 +70,33 @@ class BinaryTree:
         self.key = obj
     def getRootVal(self):
         return self.key
-# %%
+# %%解析树
+# 1.栈实现
+from stack import Stack
+from trees import BinaryTree
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in '+-*/)':
+            currentTree.setRootVal(eval(i))
+            parent = pStack.pop()
+            currentTree = parent
+        elif i in '+-*/':
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = pStack.pop()
+        else:
+            raise ValueError("Unknown Operator: " + i)
+    return eTree
+
