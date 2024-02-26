6ce7d1234dc5b3b011534ec9c9f38f3d31ca9cb4
Zhang Chao (BEG/ESF3) <chao.zhang2@de.bosch.com>
2024-02-26 08:57:16 +0100

    2024.02.26

--- binSearchTree.py
+++ binSearchTree.py
@@ -2,13 +2,10 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
         self.size = 0
-
     def length(self):
         return self.size
-
     def __len__(self):
         return self.size
-
     def __iter__(self):
         return self.root.__iter__()
 
@@ -30,18 +27,14 @@ class TreeNode:
         return self.parent and self.parent.leftChild == self
     def isRightChild(self):
         return self.parent and self.parent.rightChild == self
-
     def isRoot(self):
         return not self.parent
-
     def isLeaf(self):
         return not (self.rightChild or self.leftChild)
-
     def hasAnyChildren(self):
         return self.rightChild or self.leftChild
     def hasBothChildren(self):
         return self.rightChild and self.leftChild
-
     def replaceNodeData(self, key, value, lc, rc):
         self.key = key
         self.payload = value
@@ -51,3 +44,175 @@ class TreeNode:
             self.leftChild.parent = self
         if self.hasRightChild():
             self.rightChild.parent = self
+
+    def put(self, key, val):
+            """
+            检查树是否已经有根节点，
+            若没有，就创建一个 TreeNode,并将其作为树的根节点;
+            若有,就调用私有的递归辅助函数_put,并根据以下算法在树中搜索。
+             从根节点开始搜索二叉树，比较新键与当前节点的键。如果新键更小，搜索左子树。如
+            果新键更大，搜索右子树。
+             当没有可供搜索的左（右）子节点时，就说明找到了新键的插入位置。
+             向树中插入一个节点，做法是创建一个 TreeNode 对象，并将其插入到前一步发现的位
+            置上。
+            """
+            if self.root:
+                self._put(key, val, self.root)
+            else:
+                self.root = TreeNode(key, val)
+            self.size = self.size + 1
+
+    def _put(self, key, val, currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._put(key, val, currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
+        else:
+            if currentNode.hasRightChild():
+                self._put(key, val, currentNode.rightChild)
+            else:
+                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
+    def __setitem__(self, k, v):
+        """
+        调用 put 方法来重载[]运算符。
+        如此一来，就可以写出像 myZipTree['Plymouth'] = 55446 这样的 Python 语句，就如同访
+        问 Python 字典一样。
+        """
+        self.put(k, v)
+
+    def get(self, key):
+        if self.root:
+            res = self._get(key, self.root)
+            if res: 
+                return res.payload
+            else:
+                return None
+        else:
+            return None
+    
+    def _get(self, key, currentNode):
+        if not currentNode:
+            return None
+        elif currentNode.key == key:
+            return currentNode
+        elif key < currentNode.key:
+            return self._get(key, currentNode.leftChild)
+        else:
+            return self._get(key, currentNode.rightChild)
+        
+    def __getitem__(self, key):
+        return self.get(key)
+    
+    def __contains__(self, key):
+        """
+        __contains__方法重载了 in 运算符，因此我们可以写出这样的语句：
+        if 'Northfield' in myZipTree:
+        print("oom ya ya")
+        """
+        if self._get(key, self.root):
+           return True
+        else:
+           return False
+    def delete(self, key):
+        if self.size > 1:
+            nodeToRemove = self._get(key, self.root)
+            if nodeToRemove:
+                self.remove(nodeToRemove)
+                self.size = self.size - 1
+            else:
+                raise KeyError('Error, key not in tree')
+        elif self.size == 1 and self.root.key == key:
+                self.root = None
+                self.size = self.size - 1
+        else:
+            raise KeyError('Error, key not in tree')
+       
+    def __delitem__(self, key):
+        self.delete(key) 
+
+    def find_successor(self):
+        succ = None
+        if self.has_right_child():
+            succ = self.right_child.find_min()
+        else:
+            if self.parent:
+                if self.is_left_child():
+                    succ = self.parent
+                else:
+                    self.parent.right_child = None
+                    succ = self.parent.find_successor()
+                    self.parent.right_child = self
+        return succ
+    
+    def splice_out(self):
+        if self.is_leaf():
+            if self.is_left_child():
+                self.parent.left_child = None
+            else:
+                self.parent.right_child = None
+        elif self.has_any_children():
+            if self.has_left_child():
+                if self.is_left_child():
+                    self.parent.left_child = self.left_child
+                else:
+                    self.parent.right_child = self.left_child
+                self.left_child.parent = self.parent
+            else:
+                if self.is_left_child():
+                    self.parent.left_child = self.right_child
+                else:
+                    self.parent.right_child = self.right_child
+                self.right_child.parent = self.parent
+
+    def find_min(self):
+        current = self
+        while current.has_left_child():
+            current = current.left_child
+        return current
+    def remove(self, currentNode):
+        if currentNode.isLeaf():# 叶节点
+            if currentNode == currentNode.parent.leftChild:
+                currentNode.parent.leftChild = None
+            else:
+                currentNode.parent.rightChild = None 
+        elif currentNode.has_both_children(): #有两个子节点
+            succ = currentNode.find_successor()
+            succ.splice_out()
+            currentNode.key = succ.key
+            currentNode.payload = succ.payload
+
+        else: # 只有一个子节点
+            if currentNode.hasLeftChild():
+                if currentNode.isLeftChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.leftChild
+                elif currentNode.isRightChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.leftChild
+                else:
+                    currentNode.replaceNodeData(currentNode.leftChild.key,
+                                                currentNode.leftChild.payload,
+                                                currentNode.leftChild.leftChild,
+                                                currentNode.leftChild.rightChild)
+            else:
+                if currentNode.isLeftChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.rightChild
+                elif currentNode.isRightChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.rightChild
+                else:
+                    currentNode.replaceNodeData(currentNode.rightChild.key,
+                                                currentNode.rightChild.payload,
+                                                currentNode.rightChild.leftChild,
+                                                currentNode.rightChild.rightChild)
+    def __iter__(self):
+        if self:
+            if self.hasLeftChild():
+                for elem in self.leftChild:
+                    yield elem
+            yield self.key
+            if self.hasRightChild():
+                for elem in self.rightChild:
+                    yield elem
