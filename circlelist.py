f72b3d17e9b8dfd193f26e534a4cc67f2f001af3
Zhang Chao (BEG/ESF3) <chao.zhang2@de.bosch.com>
2024-07-29 12:41:04 +0200

    2024.07.29

--- circlelist.py
+++ circlelist.py
@@ -24,7 +24,7 @@ def circular_shift_part_of_list(lst, positions, direction='right'):
     return lst
 
 # Example usage:
-original_list = ['', 'U', '', '', 'L', 'F', 'R', 'B', '', 'D', '', '']
+orientation = ['', 'U', '', '', 'L', 'F', 'R', 'B', '', 'D', '', '']
 
 def spinCube(direction,orientation):
     circlePos = [4,5,6,7]
@@ -37,4 +37,33 @@ def rollCube(orientation):
     circlePos = [4, 1, 6, 9]
     circular_shift_part_of_list(orientation,circlePos)
 
+# %%
+import re
+
+# Original string
+original_string = 'U1 B2 D3 B3 R3 L3 U1 L2 B2 R1 D3 R3 B1 L3 U2 B2 R1 D3 (18f*)'
+
+# Remove the part inside parentheses
+cleaned_string = re.sub(r'\s*\(.*\)', '', original_string)
+
+# Split the cleaned string into individual commands
+commands = cleaned_string.split()
+
+# Print the result
+print(commands)
+
+# %%
+print(original_string)
+print(orientation)
+for command in commands:
+    char = command[0]  # Get the character part of the command
+    print(command)
+    if char in orientation:
+        position = orientation.index(char)   # Find the position (1-based index)
+        if position == 9:
+            print ('spinCube',command[1])
+        elif position in [5, 7]:
+            print ('spinCube left if 7, right if 5, then rollCube, then spinCube',command[1])
+        elif position in [1, 4, 6]:
+            print(f'rollCube till {char} in position 9')
 # %%
